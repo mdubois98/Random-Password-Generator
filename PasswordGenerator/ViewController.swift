60fb7506aca235ad5d16d19ad02aeb5e353a2a5c
Student <student@bluc02mf2qcfh51.local>
2016-12-08 10:24:19 -0600

    initial Commit

--- PasswordGenerator/ViewController.swift
+++ PasswordGenerator/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var myNumberOfCharacters: UITextField!
+ 
+    @IBOutlet weak var myCharacters: UISwitch!
+    
+    @IBOutlet weak var myNumbers: UISwitch!
+    @IBOutlet weak var mySymbols: UISwitch!
+    @IBOutlet weak var myCapitalization: UISwitch!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var mySavePreferences: UIButton!
+    {
+        myCharacters.textInputMode
+        myNumbers.textInputMode
+        mySymbols.textInputMode
+        myCapitalization.textInputMode
+        
     }
-
-
+    
+    
+    
+    //random number generation
+    let randomOne = arc4random_uniform(60)
+    let randomTwo = arc4random_uniform(56)
+    let randomThree = arc4random_uniform(78)
+    let randomFour = arc4random_uniform(45)
+    let randomFive = arc4random_uniform(47)
+    let randomSix = arc4random_uniform(87)
+    let randomSeven = arc4random_uniform(34)
+    let randomEight = arc4random_uniform(29)
+    let randomNine = arc4random_uniform(67)
+    let randomTen = arc4random_uniform(62)
+    
 }
 
